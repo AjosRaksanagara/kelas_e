88df1ef7075a577437222559f74b79444cb6e2c6
Bagas <bagas@digits.id>
2024-12-05 21:29:36 +0700

    tugas 9

---
+++ folder_git/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+#     "kedelai_lokasi2": kedelai_lokasi2,
+#     "padi_lokasi3": padi_lokasi3,
+#     "kedelai_lokasi3": kedelai_lokasi3,
+#     "padi_lokasi4": padi_lokasi4,
+#     "kedelai_lokasi4": kedelai_lokasi4,
+#     "padi_lokasi5": padi_lokasi5,
+#     "kedelai_lokasi5": kedelai_lokasi5,
+# }
